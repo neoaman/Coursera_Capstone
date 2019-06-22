4b86a90ef5bcdb95640a35f0c326db62ecf3f74e
AmanNe0 <amanthe001@gmail.com>
2019-06-22 22:41:48 +0530

    I add the file Capstone_python.py using the gitblush

---
+++ Capstone_python.py
@@ -0,0 +1,6 @@
+# This Notebook is used for the Capstone Project in IBM Coursera Course.
+# This Notebook will be mainly used for the capstone project
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
